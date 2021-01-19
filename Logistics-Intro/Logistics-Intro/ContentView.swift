4abcc7d44ec1f7211d85111d24ec9e52e188f02e
Israel Manzo <israelmanzo814@gmail.com>
2021-01-19 13:48:22 -0500

    Lecture one is completed

--- Logistics-Intro/Logistics-Intro/ContentView.swift
+++ Logistics-Intro/Logistics-Intro/ContentView.swift
@@ -9,11 +9,32 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        HStack {
+            ForEach(0..<4) { index in
+                CardView(isFaceUp: false)
+            }
+        }
+        .padding()
+        .foregroundColor(.orange)
+        .font(.largeTitle)
+    }
+}
+
+struct CardView: View {
+    
+    var isFaceUp = false
+    
+    var body: some View {
         ZStack {
-            RoundedRectangle(cornerRadius: 10.0)
-            Text("😎")
-                .frame(width: 200, height: 200)
-                .padding()
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+                RoundedRectangle(cornerRadius: 10.0)
+                    .stroke(lineWidth: 3)
+                Text("😎")
+            } else {
+                RoundedRectangle(cornerRadius: 10.0).fill()
+            }
+                
         }
     }
 }
