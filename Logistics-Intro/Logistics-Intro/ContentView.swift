37772c66466383390614b80dbb86b00d8ac51602
Israel Manzo <israelmanzo814@gmail.com>
2021-01-18 22:43:57 -0500

    Starting SwiftUI 100 days

---
+++ Logistics-Intro/Logistics-Intro/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Logistics-Intro
+//
+//  Created by Israel Manzo on 1/18/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Logistics-Intro/Logistics-Intro/Logistics_IntroApp.swift
@@ -0,0 +1,17 @@
+//
+//  Logistics_IntroApp.swift
+//  Logistics-Intro
+//
+//  Created by Israel Manzo on 1/18/21.
+//
+
+import SwiftUI
+
+@main
+struct Logistics_IntroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
