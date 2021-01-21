36829b3253f7d6441dfde86b18baa302a108fd30
Israel Manzo <israelmanzo814@gmail.com>
2021-01-20 22:28:40 -0500

    Gradients

--- GuessFlag/GuessFlag/ContentView.swift
+++ GuessFlag/GuessFlag/ContentView.swift
@@ -10,8 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack {
-            Color(red: 1, green: 0.8, blue: 0).edgesIgnoringSafeArea(.all)
-            Text("Hello, world!")
+            AngularGradient(gradient: Gradient(colors: [.red, .blue, .green, .gray, .purple]), center: .center)
         }
     }
 }
