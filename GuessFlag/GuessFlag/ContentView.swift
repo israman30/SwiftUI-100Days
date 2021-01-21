5e361f50cfef66c82cdab16e21b9c37c4043ab05
Israel Manzo <israelmanzo814@gmail.com>
2021-01-20 23:10:37 -0500

    Adding assets + added basic ui

--- GuessFlag/GuessFlag/ContentView.swift
+++ GuessFlag/GuessFlag/ContentView.swift
@@ -8,20 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"]
+    var correctAnswer = Int.random(in: 0...2)
+    
     var body: some View {
-        Button(action: {
-            // action
-        }, label: {
-            HStack {
-                Image(systemName: "pencil")
-                Text("Tap me")
+        ZStack {
+            Color.blue.edgesIgnoringSafeArea(.all)
+            VStack(spacing: 30) {
+                VStack {
+                    Text("Tap the flag of")
+                    Text(countries[correctAnswer])
+                }.foregroundColor(.white)
+                ForEach(0..<3) { number in
+                    Button(action: {
+                        
+                    }, label: {
+                        Image(self.countries[number])
+                            .renderingMode(.original)
+                    })
+                }
             }
-            .font(.title)
-            .foregroundColor(.white)
-            .padding()
-            .background(Color.blue)
-            .cornerRadius(10)
-        })
+        }
+        
+        
     }
 }
 
