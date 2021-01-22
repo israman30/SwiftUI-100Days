c76b963e138985feffc24c1791efcb0406c0b76b
Israel Manzo <israelmanzo814@gmail.com>
2021-01-22 13:13:05 -0500

    Stepper with numbers

---
+++ EnteringNumbers-Stepper/EnteringNumbers-Stepper/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  EnteringNumbers-Stepper
+//
+//  Created by Israel Manzo on 1/22/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var sleepAmount = 8.0
+    var body: some View {
+        Stepper(value: $sleepAmount, in: 4...12, step: 0.25) {
+            Text("\(sleepAmount, specifier: "%g") hours")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ EnteringNumbers-Stepper/EnteringNumbers-Stepper/EnteringNumbers_StepperApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnteringNumbers_StepperApp.swift
+//  EnteringNumbers-Stepper
+//
+//  Created by Israel Manzo on 1/22/21.
+//
+
+import SwiftUI
+
+@main
+struct EnteringNumbers_StepperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
